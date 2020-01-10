aab1a2574150a3c8ff592b63d7f6d41ad14f2f35
Antoine FSG <35838884+AntoineFSG@users.noreply.github.com>
2020-01-10 08:38:48 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/IMPROPER-logo-2015short.png`, // This path is relative to the root of the site.
+        //icon: `src/images/IMPROPER-logo-2015short.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
