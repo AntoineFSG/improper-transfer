219b60a00a6bacbaf0041eced662d704fd2c5100
FIRST_NAME LAST_NAME <antoinefsg@icloud.com>
2020-01-15 20:50:48 -0800

    fixed slider bug

--- src/components/slideShow.js
+++ src/components/slideShow.js
@@ -7,10 +7,14 @@ const SlideShow = (props)=>{
   const slides= props.slides;
   //Minus 1 for array offset from 0
   const length = slides.length - 1
-  const handleNext = () =>
-    index === length ? setIndex(0) : setIndex(index + 1)
-  const handlePrevious = () =>
-    index === 0 ? setIndex(length) : setIndex(index - 1)
+  const handleNext = () =>{
+    if(slides[index+1]!=null){
+    index === length ? setIndex(0) : setIndex(index + 1)}
+    else{return}
+  }
+  const handlePrevious = () =>{if(slides[index-1]!=null){
+    index === 0 ? setIndex(length) : setIndex(index - 1)}
+    else{return}}
   const node = slides[index]
   return (
     <div className={slideShowStyles.slideWrapper}>
