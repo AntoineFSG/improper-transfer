383f4871afb3ac44bf7f3d07b2f6793bea6e4fab
Antoine FSG <35838884+AntoineFSG@users.noreply.github.com>
2020-01-26 10:42:11 -0800

    Update postlist.js

--- src/components/postlist.js
+++ src/components/postlist.js
@@ -9,18 +9,19 @@ import postListStyles from "../css/postList.module.scss"
 class PostList extends Component {
   render() {
     const data = this.props.data
+    
+    
 
     return (
       <>
         <div className={postListStyles.mainPostContainer}>
-        {data.edges.map(({ node }) => (
+        {data.map(({ node }) => (
           <div className={postListStyles.postContainer}key={node.slug}>
           {node.featured_media && <Img fluid={node.featured_media.localFile.childImageSharp.fluid}/>}
             <Link className={postListStyles.overLay} to={node.slug}>
               <h2 className={postListStyles.title}><Parser data={node.title}/></h2>
-              <p><Parser data={node.excerpt}/></p>
+              <div><Parser data={node.excerpt}/></div>
             </Link>
-           
           </div>
         ))}
         </div>
